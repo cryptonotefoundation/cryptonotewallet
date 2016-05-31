e9c36a5acd2aa965fe7d15679679594c9704e814
Taras Sereda <seredat@protonmail.com>
2016-05-31 08:21:20 +0300

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "karbowanec")
+set(CN_CURRENCY_DISPLAY_NAME "Карбованець")
+set(CN_CURRENCY_TICKER "KRB")
