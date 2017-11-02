07d297fe96c1313cf91894140470a28c1da9f1cd
valiant1x <valiant1x@users.noreply.github.com>
2017-11-01 22:07:17 -0400

    version bump

--- src/Settings/Settings.cpp
+++ src/Settings/Settings.cpp
@@ -71,7 +71,7 @@ const quint64 DEFAULT_OPTIMIZATION_MIXIN = 6;
 
 const quint64 VERSION_MAJOR = 1;
 const quint64 VERSION_MINOR = 2;
-const quint64 VERSION_PATCH = 2;
+const quint64 VERSION_PATCH = 3;
 
 }
 
