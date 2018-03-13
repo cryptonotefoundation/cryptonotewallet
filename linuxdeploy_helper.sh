74c1bd2221c74b85d7b94a5f04d91a092c4450cf
Piero Orsoni <pallas1@users.noreply.github.com>
2018-03-13 11:32:39 +0100

    Add missing libraries and avoid copying gpu-specific libGL

--- linuxdeploy_helper.sh
+++ linuxdeploy_helper.sh
@@ -30,15 +30,16 @@ if [ -z "$QTXML_DIR" ]; then
 fi
 
 # Copy dependencies
-EXCLUDE='libstdc++|libgcc_s.so|libc.so|libpthread'
+EXCLUDE='libstdc++|libgcc_s.so|libc.so|libpthread|libdl.so|libm.so|libGL.so'
 INCLUDE='libunbound'
 cp -rv $QT_DIR/qml $TARGET || exit
 cp -rv $QT_DIR/plugins $TARGET || exit
 mkdir -p $TARGET/libs || exit
-#ldd $TARGET/$GUI_EXEC | grep "=> /" | awk '{print $3}' | grep $INCLUDE | xargs -I '{}' cp -v '{}' $TARGET/libs || exit
-#ldd $TARGET/$GUI_EXEC | grep "=> /" | awk '{print $3}' | grep -Ev $EXCLUDE | xargs -I '{}' cp -v '{}' $TARGET/libs || exit
+ldd $TARGET/$GUI_EXEC | grep "=> /" | awk '{print $3}' | grep $INCLUDE | xargs -I '{}' cp -v '{}' $TARGET/libs || exit
+ldd $TARGET/$GUI_EXEC | grep "=> /" | awk '{print $3}' | grep -Ev $EXCLUDE | xargs -I '{}' cp -v '{}' $TARGET/libs || exit
 #ldd $TARGET/plugins/platforms/libqxcb.so| grep "=> /" | awk '{print $3}' | grep -Ev $EXCLUDE | xargs -I '{}' cp -v '{}' $TARGET/libs || exit
-#cp -v $QTXML_DIR/libQt5XmlPatterns.so.5 $TARGET/libs || exit
+ldd `find $TARGET/plugins $TARGET/qml -name \*.so`| grep "=> /" | awk '{print $3}' | grep -Ev $EXCLUDE | xargs -I '{}' cp -v '{}' $TARGET/libs || exit
+cp -v $QTXML_DIR/libQt5XmlPatterns.so.5 $TARGET/libs || exit
 
 # Create start script
 cat > $TARGET/start-gui.sh <<EOL
