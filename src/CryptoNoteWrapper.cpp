aa56934d3a4cd0936e41611d29e4dd537d904455
Aiwe <maksymivv@gmail.com>
2018-04-05 14:36:28 +0300

    Fix checkpoints Update min. tx fee and max. mixin

--- src/CryptoNoteWrapper.cpp
+++ src/CryptoNoteWrapper.cpp
@@ -435,12 +435,17 @@ public:
     m_nodeServer(m_dispatcher, m_protocolHandler, logManager),
     m_node(m_core, m_protocolHandler) {
 
-    m_core.set_cryptonote_protocol(&m_protocolHandler);
-    m_protocolHandler.set_p2p_endpoint(&m_nodeServer);
-    CryptoNote::Checkpoints checkpoints(logManager);
-    for (const CryptoNote::CheckpointData& checkpoint : CryptoNote::CHECKPOINTS) {
-      checkpoints.add_checkpoint(checkpoint.height, checkpoint.blockId);
-    }
+      CryptoNote::Checkpoints checkpoints(logManager);
+      for (const CryptoNote::CheckpointData& checkpoint : CryptoNote::CHECKPOINTS) {
+        checkpoints.add_checkpoint(checkpoint.height, checkpoint.blockId);
+      }
+      if (!Settings::instance().isTestnet()) {
+        m_core.set_checkpoints(std::move(checkpoints));
+      }
+
+      m_core.set_cryptonote_protocol(&m_protocolHandler);
+      m_protocolHandler.set_p2p_endpoint(&m_nodeServer);
+
   }
 
   ~InprocessNode() override {
