b021e8cec9a35ee211bae1d3cab280af98ed0d81
Laion Camargo <laion.camargo@alfasoft.pt>
2018-06-20 10:58:07 -0300

    add config file

---
+++ IntenseConfig.js
@@ -0,0 +1,15 @@
+
+var url = "https://jhx4eq5ijc.execute-api.us-east-1.amazonaws.com"
+var version = "/v1"
+var stage = "/dev"
+var services = "/services"
+var feedback = "/feedback"
+var setup = "/setup"
+var add = "/add"
+var get = "/get"
+var search = "/search"
+var jsonRpcURL = "http://itns.alfasoft.pt:3000/json_rpc"
+var wallet = "iz5RCx5nsRAdvpfGnTjqB4Q8rv5zKkvJS1skjD6m7w2pdGbSX44QsETVK6Gcrgz6U99Ar4o3a8SMFQPzzC7tJ64H1bZcfgYAJ"
+var payTimer = 20000 // 60000 = 1 minute
+var haproxyIp = "127.0.0.1"
+var haproxyPort = "6666"
