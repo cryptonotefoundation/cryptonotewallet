9809e9b3dd891e19f6f207f51220b79612122c86
Laion Camargo <laion.camargo@alfasoft.pt>
2018-07-03 16:41:44 -0300

    add endpoint and port to haproxy config file

--- src/libwalletqt/Haproxy.cpp
+++ src/libwalletqt/Haproxy.cpp
@@ -6,7 +6,7 @@
 #include <string>
 //#include <stdlib.h>
 
-void Haproxy::haproxy(const QString &host, const QString &ip, const QString &port){
+void Haproxy::haproxy(const QString &host, const QString &ip, const QString &port, const QString &endpoint, const QString &endpointport){
     QFile file (host+"/haproxy.cfg");
     if(!file.exists()){
         qDebug() << file.fileName() << "does not exists";
@@ -15,39 +15,43 @@ void Haproxy::haproxy(const QString &host, const QString &ip, const QString &por
         QTextStream txtStream(&file);
         qDebug() << "---Writing to file---";
         txtStream << "global\n";
-        txtStream << "maxconn         512\n";
+        txtStream << "maxconn         2000\n";
         txtStream << "daemon\n";
-        //txtStream << "ssl-default-bind-ciphers ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:RSA+AESGCM:RSA+AES:!aNULL:!MD5:!DSS\n";
-        //txtStream << "ssl-default-bind-options no-sslv3\n";
-        txtStream << "frontend http-in"
+        txtStream << "ssl-default-bind-ciphers ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:RSA+AESGCM:RSA+AES:!aNULL:!MD5:!DSS\n";
+        txtStream << "ssl-default-bind-options no-sslv3\n";
+        txtStream << "frontend icproxy"
                      "\n";
         txtStream << "bind            "+ip+":"+port+"\n";
         txtStream << "mode            http\n";
-        //txtStream << "log             global\n";
-        //txtStream << "option          httplog\n";
-        //txtStream << "option          dontlognull\n";
-        //txtStream << "option          nolinger\n";
+        txtStream << "log             global\n";
+        txtStream << "option          httplog\n";
+        txtStream << "option          dontlognull\n";
+        txtStream << "option          nolinger\n";
         txtStream << "option          http_proxy\n";
-        //txtStream << "option          contstats\n";
-        //txtStream << "maxconn         8000\n";
+        txtStream << "option          contstats\n";
+        txtStream << "maxconn         8000\n";
         txtStream << "timeout client  30s\n";
         txtStream << "default_backend http-proxy\n";
         txtStream << "backend http-proxy\n";
         txtStream << "mode            http\n";
         txtStream << "timeout connect 5s\n";
         txtStream << "timeout server  5s\n";
-        //txtStream << "retries         2\n";
-        //txtStream << "option          nolinger\n";
-        //txtStream << "option          httplog\n";
-        //txtStream << "http-request add-header X-ITNS-PaymentID authid1\n";
-        txtStream << "server monitor.intensecoin.com:20000 '"+host+"/ca.cert.pem'";
+        txtStream << "retries         2\n";
+        txtStream << "option          nolinger\n";
+        txtStream << "option          httplog\n";
+        txtStream << "http-request add-header X-ITNS-PaymentID authid1\n";
+        //
+        txtStream << "server hatls " + endpoint + ":" + endpointport + " ssl ca-file '"+host+"/ca.cert.pem'";
+
 
         qDebug() << " ----- reading from file ------";
+
         txtStream.seek(0);
         while(!txtStream.atEnd()){
             qDebug() << txtStream.readLine();
         }
         file.close();
+
         #ifdef Q_OS_WIN
             qDebug() << " ----- run windows haproxy ------";
             const QString command=host+"haproxy.exe -f "+host+"/haproxy.cfg";
