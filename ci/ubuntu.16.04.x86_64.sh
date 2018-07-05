1c7019b6af102dc79750791e30cc8db2bd52bdd7
Michael Iedema <michael@kapsulate.com>
2018-07-05 15:28:24 +0200

    add qml-module-qt-labs-settings to Ubuntu build deps

--- ci/ubuntu.16.04.x86_64.sh
+++ ci/ubuntu.16.04.x86_64.sh
@@ -32,6 +32,7 @@ if [ "$1" = "prep" ]; then
 	installDependencyIfNeeded qml-module-qtquick-xmllistmodel
 	installDependencyIfNeeded qttools5-dev-tools
 	installDependencyIfNeeded qml-module-qtquick-dialogs
+	installDependencyIfNeeded qml-module-qt-labs-settings
 	# - temporary link workaround
 	installDependencyIfNeeded libunbound-dev
 
