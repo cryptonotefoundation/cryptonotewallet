9c079e2d6a0e48d4a7ee6fd4ed37b53690fc3c47
Aiwe <maksymivv@gmail.com>
2019-04-24 15:28:41 +0300

    Update v. 1.3.4

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,5 +1,5 @@
 
-set(CN_PROJECT_NAME "karbowanec")
+set(CN_PROJECT_NAME "KarbowanecWallet")
 set(CN_CURRENCY_DISPLAY_NAME "Karbo")
 set(CN_CURRENCY_TICKER "KRB")
-set(CN_VERSION 1.3.3)
+set(CN_VERSION 1.3.4)
--- src/CryptoNoteWrapper.cpp
+++ src/CryptoNoteWrapper.cpp
@@ -4,6 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "CryptoNoteWrapper.h"
+#include <CheckpointsData.h>
 #include "CryptoNoteCore/CryptoNoteBasicImpl.h"
 #include "CryptoNoteCore/CryptoNoteFormatUtils.h"
 #include "CryptoNoteCore/Currency.h"
