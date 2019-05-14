9186974dc27aa0ea149ba5d27a81c39b1811f7f5
Aiwe <maksymivv@gmail.com>
2019-05-14 16:26:59 +0300

    Use last wallet's containing folder when opening wallet

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -386,13 +386,19 @@ void MainWindow::createNonDeterministicWallet() {
 }
 
 void MainWindow::openWallet() {
-  QString filePath = QFileDialog::getOpenFileName(this, tr("Open .wallet/.keys file"),
+  QString walletDirectory = "";
+  QString lastwalletDir = QFileInfo(Settings::instance().getWalletFile()).absolutePath();
+  if (!lastwalletDir.isEmpty()) {
+    walletDirectory = lastwalletDir;
+  } else {
 #ifdef Q_OS_WIN
-    //QApplication::applicationDirPath(),
-      QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation),
+    walletDirectory = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
 #else
-    QDir::homePath(),
+    walletDirectory = QDir::homePath();
 #endif
+  }
+  QString filePath = QFileDialog::getOpenFileName(this, tr("Open .wallet/.keys file"),
+    walletDirectory,
     tr("Wallet (*.wallet *.keys)"));
 
   if (!filePath.isEmpty()) {
