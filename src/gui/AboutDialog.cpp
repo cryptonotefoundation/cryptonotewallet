45d70af2dafeb2970735d0c26fd3ab562f23a724
Aiwe <maksymivv@gmail.com>
2019-07-26 17:26:06 +0300

    Core update and display its version in About dialog

--- src/gui/AboutDialog.cpp
+++ src/gui/AboutDialog.cpp
@@ -5,6 +5,8 @@
 #include "AboutDialog.h"
 #include "CurrencyAdapter.h"
 #include "Settings.h"
+#include "version.h"
+#include "CryptoNoteWalletConfig.h"
 
 #include "ui_aboutdialog.h"
 
@@ -15,6 +17,7 @@ AboutDialog::AboutDialog(QWidget* _parent) : QDialog(_parent), m_ui(new Ui::Abou
   setWindowTitle(QString(tr("About %1 Wallet")).arg(CurrencyAdapter::instance().getCurrencyDisplayName()));
   QString aboutText = m_ui->m_aboutLabel->text();
   m_ui->m_aboutLabel->setText(aboutText.arg(VERSION));
+  m_ui->m_aboutLabel->setText(aboutText.arg(VERSION " (" GIT_REVISION ")").arg(PROJECT_VERSION_LONG));
 }
 
 AboutDialog::~AboutDialog() {
