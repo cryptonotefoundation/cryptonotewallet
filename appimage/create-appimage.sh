ec13deaf27c8bef2c0c01df21c466ee9796249af
Patrick McGuire <insidenothing@gmail.com>
2022-01-17 22:24:20 -0500

    Update create-appimage.sh

--- appimage/create-appimage.sh
+++ appimage/create-appimage.sh
@@ -7,8 +7,8 @@ if ! test -f "linuxdeploy-plugin-qt-x86_64.AppImage"; then
     wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
 fi
 chmod u+x ./*.AppImage
-cp ../build/release/CONCEAL-GUI .
+cp ../build/release/bmorecoin .
 mkdir -p usr/share/icons
 mkdir -p usr/lib
-cp ../src/images/conceal.png usr/share/icons
-./linuxdeploy-x86_64.AppImage --executable ./CONCEAL-GUI --desktop-file conceal-desktop.desktop --appdir . --output appimage --plugin qt
+cp ../src/images/bmorecoin.png usr/share/icons
+./linuxdeploy-x86_64.AppImage --executable ./bmorecoin --desktop-file bmorecoin-desktop.desktop --appdir . --output appimage --plugin qt
